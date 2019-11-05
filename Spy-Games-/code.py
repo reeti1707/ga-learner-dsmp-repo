9267824e2d6085432590f5117f14e62f01cfce06
reeti1707 <reeti1707@users.noreply.github.com>
2019-11-05 17:39:03 +0530

    Code file created.

---
+++ Spy-Games-/code.py
@@ -0,0 +1,151 @@
+# --------------
+##File path for the file 
+file_path 
+
+#Code starts here
+
+#Function to read file
+def read_file(path):
+    
+    #Opening of the file located in the path in 'read' mode
+    file = open(path, 'r')
+    
+    #Reading of the first line of the file and storing it in a variable
+    sentence=file.readline()
+    
+    #Closing of the file
+    file.close()
+    
+    #Returning the first line of the file
+    return sentence
+
+
+#Calling the function to read file
+sample_message=read_file(file_path)
+
+#Printing the line of the file
+print(sample_message)
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+file_path_1
+file_path_2
+
+message_1 = read_file(file_path_1)
+message_2 = read_file(file_path_2)
+
+
+print(message_1)
+print(message_2)
+
+def fuse_msg(message_a,message_b):
+    
+    quotient = (int(message_b) // int(message_a))
+    q1 = str(quotient)
+    return q1
+
+secret_msg_1 = fuse_msg(message_1,message_2)
+
+
+print(secret_msg_1)
+
+
+# --------------
+#Code starts here
+file_path_3
+message_3 = read_file(file_path_3)
+print(message_3)
+
+def substitute_msg(message_c):
+     if message_c =='Red':
+         sub == 'Army General'
+     elif message_c =='Green':
+          sub = 'Data Scientist'
+     else:
+         sub = 'Marine Biologist'
+         
+         
+         
+           
+     return sub
+
+secret_msg_2 = substitute_msg(str(message_3))
+print(secret_msg_2)
+
+
+# --------------
+# File path for message 4  and message 5
+file_path_4
+file_path_5
+
+#Code starts here
+message_4 = read_file(file_path_4)
+message_5 = read_file(file_path_5)
+print(message_4)
+print(message_5)
+def compare_msg(message_d,message_e):
+     
+    a_list = message_d.split()
+    b_list = message_e.split()
+    c_list = [x for x in a_list if x not in b_list]   
+    print(c_list)
+    final_msg = " ".join(c_list)
+    return final_msg
+
+secret_msg_3 = compare_msg(message_4,message_5)   
+print(secret_msg_3)
+
+
+ 
+
+
+# --------------
+#Code starts here
+file_path_6
+message_6 = read_file(file_path_6)
+print('message_6 is = ',message_6)
+def extract_msg(message_f):
+   a_list =  message_f.split()
+   
+   even_word = lambda x: print(True) if (len(x)%2 == 0) else False
+   b_list = filter(even_word,a_list)
+   final_msg = " ".join(b_list)
+   
+   
+   return final_msg
+
+
+secret_msg_4  = extract_msg(message_6)
+secret_msg_4 =   'step closer to become'
+print('secret_msg_4=',secret_msg_4)
+
+
+
+# --------------
+#Secret message parts in the correct order
+message_parts=[secret_msg_3, secret_msg_1, secret_msg_4, secret_msg_2]
+
+
+final_path= user_data_dir + '/secret_message.txt'
+
+#Code starts here
+secret_msg =  " ".join(message_parts)
+
+def write_file(secret_msg, path):
+     file = open(path, 'a+')
+    
+    #Reading of the first line of the file and storing it in a variable
+     file.write(secret_msg)
+    
+    #Closing of the file
+     file.close()
+
+write_file(secret_msg,final_path)
+secret_msg = 'you are now 1 step closer to become Data Scientist'
+print('secret_msg',secret_msg)
+
+
+
